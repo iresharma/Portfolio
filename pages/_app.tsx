b675edd243caa17587c6443a5dabd8053f16b71f
iresharma <iresh.sharma8@gmail.com>
2025-01-22 03:54:15 +0530

    Vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,10 @@
 import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import type {AppProps} from "next/app";
+import {SpeedInsights} from '@vercel/speed-insights/next';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+export default function App({Component, pageProps}: AppProps) {
+    return <>
+        <Component {...pageProps} />
+        <SpeedInsights/>
+    </>;
 }
